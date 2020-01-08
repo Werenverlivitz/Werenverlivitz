05c6f37aa3f520f114c705d0a4fb046e994a2692
Werenverlivitz <pacguy.123456789@gmail.com>
2020-01-08 20:42:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+app = require('express')()
+http = require('http').Server(app)
+io = require('socket.io')(http)
 
 app.get('/', function(req, res) {
    res.sendfile('index.html')
-});
+})
+
+io.listen(process.env.PORT || 2000)
+console.log("Server started.")
 
 io.on('connection', function(socket) {
    socket.on("msg",(e)=>{
       socket.emit("msg",{data:e.data})
    })
 })
-
-io.listen(process.env.PORT || 2000)
-console.log("Server started.")
