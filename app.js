8484a3b3e5f77c4137d8f56f47b3b1d7f09fbb71
Werenverlivitz <pacguy.123456789@gmail.com>
2020-01-08 20:42:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ app = require('express')()
 http = require('http').Server(app)
 io = require('socket.io')(http)
 
-app.get('/', function(req, res) {
+app.get('/', (req, res)=>{
    res.sendfile('index.html')
 })
 
 io.listen(process.env.PORT || 2000)
 console.log("Server started.")
 
-io.on('connection', function(socket) {
+io.on('connection', (socket)=>{
    socket.on("msg",(e)=>{
       socket.emit("msg",{data:e.data})
    })
