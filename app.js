f86d3667e940c511814f18a90de9e097b8a17ce5
Werenverlivitz <pacguy.123456789@gmail.com>
2020-01-16 17:58:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-express = require('express')
-app = express()
-http = require('http').createServer(app)
-io = require('socket.io')(http)
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
 
-app.get('/',function (req,res){
-   res.sendFile(__dirname+'/index.html')
-})
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
 
-app.use('/client',express.static(__dirname+'/'))
-
-io.listen(process.env.PORT||2000)
+serv.listen(process.env.PORT || 2000);
+console.log("Server started.");
 
 i1=0
 obj={}
-io.on('connection', function (socket){
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
    i1++
    obj[i1]={msg:""}
    socket.on("msg",(e)=>{
