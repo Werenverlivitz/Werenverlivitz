fb29a225e16c0b7a78d5859716be6f6845df41c4
Werenverlivitz <pacguy.123456789@gmail.com>
2020-01-24 23:53:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,19 @@ obj={}
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
    i1++
-   obj[i1]={msg:""}
+   obj[i1]={iron:0,uranium:0,drillers:0,trucks:0,nukes:0}
    socket.on("msg",(e)=>{
       obj[i1].msg=e.data
    })
    setInterval(()=>{
       for(i in obj){
-         socket.emit("msg",{user:i,data:obj[i].msg})
+         socket.emit("msg",{
+		 user:i,iron:obj[i].iron,
+		 uranium:obj[i].uranium,
+		 drillers:obj[i].drillers,
+		 trucks:obj[i].trucks,
+		 nukes:obj[i].nukes
+	 })
       }
    },1)
 })
