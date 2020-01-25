40c4600f920cce8b2107511f30dd80f66ecd5bc3
Werenverlivitz <pacguy.123456789@gmail.com>
2020-01-25 04:00:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,20 +24,20 @@ io.sockets.on('connection', function(socket){
    socket.on("build",(e)=>{
       for(i=0;i<e.quantity;i++){
         if(e.data=="drillers"&&100<=obj[i1].iron){
-          obj[i].iron-=100
+          obj[i1].iron-=100
           obj[i1].drillers++
         }
         if(e.data=="trucks"&&20<=obj[i1].iron){
-          obj[i].iron-=20
+          obj[i1].iron-=20
           obj[i1].trucks++
         }
         if(e.data=="teslatowers"&&20<=obj[i1].iron){
-          obj[i].iron-=20
+          obj[i1].iron-=20
           obj[i1].teslatowers++
         }
         if(e.data=="nukes"&&20<=obj[i1].iron&&5<=obj[i1].uranium){
-          obj[i].iron-=20
-          obj[i].uranium-=5
+          obj[i1].iron-=20
+          obj[i1].uranium-=5
           obj[i1].nukes++
         }
       }
