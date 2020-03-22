4b5873171f8c2242b5499a0454ba8a840425fdcd
Werenverlivitz <pacguy.123456789@gmail.com>
2020-03-22 02:34:27 +0100

    Update ☭.js

--- docs/☭.js
+++ docs/☭.js
@@ -1,7 +1,9 @@
-f=(p1)=>{
+f=(p1,p2,p3)=>{
   if(p1=="☭"){
-    //The script executor must accept communism!!! >:(
+    //The script executor must accept communism!!! >:|
+    
   }else{
+    //If the script executor doesnt accept communism must get outta banned here!!!! >:'U
     console.error("Capitalist idiots banned here.")
   }
 }
