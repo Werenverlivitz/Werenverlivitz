9973ec8b5eec56e3ccfce412027d9ae412b192fe
Werenverlivitz <pacguy.123456789@gmail.com>
2020-03-22 03:26:22 +0100

    Update ☭.js

--- docs/☭.js
+++ docs/☭.js
@@ -9,12 +9,16 @@ f=(operation,operand1)=>{
     case "rend":
       r=()=>{requestAnimationFrame(this);eval(operand1)};r();
       break;
+    case "log":
+      console.log(operand1)
+      break;
     default:
       console.warn("Incorrect command.")
       console.log("Showing available commands here ;) : ")
       console.log("get: gets the element of a part of your document")
       console.log("calc: executes a line of javascript code")
       console.log("rend: creates a render function executing a line of javascript code")
+      console.log("log: log a text string in the console")
       break;
   }
 }
