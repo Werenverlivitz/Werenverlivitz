ccf21ac743bb39cfb0e8ac0401e6ff3614580daf
Werenverlivitz <pacguy.123456789@gmail.com>
2020-03-22 03:27:52 +0100

    Update ☭.js

--- docs/☭.js
+++ docs/☭.js
@@ -1,17 +1,20 @@
-f=(operation,operand)=>{
+f=(operation,operand1,operand2)=>{
   switch(operation){
     case "get":
-      return document.getElementById(operand);
-      break;
+      return document.getElementById(operand)
+      break
     case "calc":
-      return eval(operand);
-      break;
+      return eval(operand)
+      break
     case "rend":
-      r=()=>{requestAnimationFrame(this);eval(operand)};r();
-      break;
+      r=()=>{requestAnimationFrame(this);eval(operand)};r()
+      break
     case "log":
       console.log(operand)
       break;
+    case "pow":
+      console.log(operand2)
+      break
     default:
       console.warn("Incorrect command.")
       console.log("Showing available commands here ;) : ")
@@ -19,6 +22,7 @@ f=(operation,operand)=>{
       console.log("calc: executes a line of javascript code")
       console.log("rend: creates a render function executing a line of javascript code")
       console.log("log: log a text string in the console")
-      break;
+      console.log("pow: calculates a pow between 2 numbers")
+      break
   }
 }
