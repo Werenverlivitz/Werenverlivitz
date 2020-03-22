2cd88c9d9c8ed448afa641e6dba13f9a1253688d
Werenverlivitz <pacguy.123456789@gmail.com>
2020-03-22 02:40:59 +0100

    Update ☭.js

--- docs/☭.js
+++ docs/☭.js
@@ -1,11 +1,11 @@
 f=(politics,operation,operand1)=>{
   if(politics=="☭"){
     //The script executor must accept communism!!! >:|
-    if(operation=="calc"){
-      return eval(operand1)
-    }
     if(operation=="get"){
       return document.getElementById(operand1)
+    }
+    if(operation=="calc"){
+      return eval(operand1)
     }
     if(operation=="rend"){
       return (()=>{requestAnimationFrame(this);eval(operand1)})
