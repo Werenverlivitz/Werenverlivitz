22d0404b5c2ba3d1b46d0a4e2ad57c09da5e183d
Werenverlivitz <pacguy.123456789@gmail.com>
2020-05-16 22:32:03 +0200

    Create predict-function.js

---
+++ docs/predict-function.js
@@ -0,0 +1,17 @@
+s1=document.createElement("script")
+s1.src="https://werenverlivitz.github.io/werenverlivitz/brain-controller-v2.js"
+s1.appendChild(s1)
+
+predict=(training,steps)=>{
+    //Init the training network:
+    net1=new brain.recurrent.RNNTimeStep()
+
+    //Init the training data:
+    training=[1,2,3]
+
+    //Train the net:
+    net1.train([training])
+
+    //Get the prediction:
+    return net1.forecast(training,steps)
+}
