62ff559e14b603e4afb0526c6027378952860f72
Werenverlivitz <pacguy.123456789@gmail.com>
2020-08-19 23:22:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,47 @@ console.log("Server started.")
 var io = require('socket.io')(serv,{})
 io.sockets.on('connection', (socket)=>{
   socket.on("msg",(e)=>{
-    socket.broadcast.emit("msg",{data:e.data})
+    if(contiene_insultos(e.data)){
+      //Ha insultado.
+    }else{
+      //No ha insultado.
+      socket.broadcast.emit("msg",{data:e.data})
+    }
   })
 })
+contiene_insultos=(msg1)=>{
+	insultos=[
+		"put",
+		"cabron",
+		"maric",
+		"jod",
+		"poll",
+		"retrasad",
+		"gut",
+		"noob",
+		"whore",
+		"bitch",
+		"suck",
+		"retard",
+		"dumb",
+		"idiot",
+		"gay",
+		"lamb",
+		"borrego",
+		"huev",
+		"perolo",
+		"chot",
+		"tranc",
+		"pij",
+		"verg",
+		"pendej"
+	]
+	msg1=msg1.toLowerCase()
+	contiene_insultos_var=false
+	for(i in insultos){
+		if(msg1.includes(insultos[i])){
+			contiene_insultos_var=true
+		}
+	}
+	return contiene_insultos_var
+}
